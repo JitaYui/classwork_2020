c345a0663ea16640f79ff1a79798aae72c748434
JItayui <shugochara13456@gmail.com>
2020-06-01 22:01:02 +0800

    set Modbus function be Thread

--- main.py
+++ main.py
@@ -5,26 +5,29 @@ import time
 import modbus_tk.modbus_tcp as mt
 import modbus_tk.defines as md
 import pymysql
-
+from threading import Thread
 
 tk = Tk()
 ip = StringVar()
 slaid = IntVar()
+slaid.set(1)
 addr = IntVar()
+addr.set(18176)
 quan = IntVar()
-#rate = IntVar()
+quan.set(2)
+# rate = IntVar()
 port = IntVar()
+port.set(502)
 timeout = IntVar()
-"""reset"""
+timeout.set(1000)
 
-
-def createFuctionWindow():
+def createFunctionWindow():
     nw = Toplevel(tk)
     frame1 = Frame(nw, padding=10)
     frame1.pack()
     l1 = Label(frame1, text="Slave ID")
     t1 = Entry(frame1, textvariable=slaid)
-    b1 = Button(frame1, text="OK")
+    b1 = Button(frame1, text="OK", command=lambda: nw.destroy())
     b1.grid(row=0, column=2, padx=5, pady=2)
     l2 = Label(frame1, text="Function")
     text1 = "03 Read Holding Registers(4x)"
@@ -36,8 +39,8 @@ def createFuctionWindow():
     t3 = Entry(frame1, textvariable=addr)
     l4 = Label(frame1, text="Quantity")
     t4 = Entry(frame1, textvariable=quan)
-    #l5 = Label(frame1, text="Scan Rate")
-    #t5 = Entry(frame1, textvariable=rate)
+    # l5 = Label(frame1, text="Scan Rate")
+    # t5 = Entry(frame1, textvariable=rate)
     b5 = Button(frame1, text="Apply")
     b5.grid(row=4, column=2, padx=5, pady=2)
     labels = [l1, l2, l3, l4]
@@ -48,27 +51,26 @@ def createFuctionWindow():
         i.grid(row=inputs.index(i), column=1, padx=5, pady=2, sticky=W)
 
 
-
 def createConnectionWindow():
     nw = Toplevel(tk)
     f1 = Frame(nw)
     f1.pack()
     lf1 = LabelFrame(f1, text="Connection", padding=10)
-    lf1.grid(row=0,column=0,rowspan=2)
+    lf1.grid(row=0, column=0, rowspan=2)
     cb1 = Combobox(lf1, state="disabled", value=("Modbus TCP/IP",), )
     cb1.current(0)
     cb1.pack()
-    b1 = Button(f1, text="OK", command= lambda: nw.destroy() )
-    b1.grid(row=0,column=1,padx=10)
-    b2 = Button(f1, text="Cancel", command= lambda: nw.destroy() )
-    b2.grid(row=1,column=1,padx=10)
+    b1 = Button(f1, text="OK", command=lambda: nw.destroy())
+    b1.grid(row=0, column=1, padx=10)
+    b2 = Button(f1, text="Cancel", command=lambda: nw.destroy())
+    b2.grid(row=1, column=1, padx=10)
     lf2 = LabelFrame(nw, text="Remote Modbus Server", padding=10)
     lf2.pack(side=TOP)
     l1 = Label(lf2, text="IP Address or Node Name")
     l1.grid(row=0, column=0, columnspan=3, sticky=W)
-    #cb2 = Entry(lf2)
-    #cb2.grid(row=1, column=0, columspan=3, sticky=W)
-    cb2 = Combobox(lf2, textvariable=ip)
+    # cb2 = Entry(lf2)
+    # cb2.grid(row=1, column=0, columspan=3, sticky=W)
+    cb2 = Combobox(lf2, textvariable=ip, value=('172.20.10.3',))
     cb2.grid(row=1, column=0, columnspan=3, sticky=W)
     l2 = Label(lf2, text="Server Port")
     l2.grid(row=2, column=0)
@@ -83,41 +85,44 @@ def createConnectionWindow():
     rb1.grid(row=2, column=2)
     rb2.grid(row=3, column=2)
 
-def Modbus():
-    IP = ip.get()
-    SLAID = slaid.get()
-    ADDR = addr.get()
-    QUAN = quan.get()
-    # RATE = rate.get()
-    PORT = port.get()
-    TIMEOUT = timeout.get() / 1000
-    # Modbus_polling
-    master = mt.TcpMaster(IP, PORT)
-    master.set_timeout(TIMEOUT)
-    if SLAID=="" or IP == "" or ADDR == "" or QUAN == "" or PORT== "" or TIMEOUT == "":
 
-        print("error")
-        F1 = "error"
-    else:
+class TModbus(Thread):
+    def __init__(self):
+        super().__init__(daemon=True)
+        self.IP = ip.get()
+        self.SLAID = slaid.get()
+        self.ADDR = addr.get()
+        self.QUAN = quan.get()
+        self.PORT = port.get()
+        self.TIMEOUT = timeout.get() / 1000
+
+        pass
+
+    def run(self):
+        master = mt.TcpMaster(self.IP, self.PORT)
+        master.set_timeout(self.TIMEOUT)
+        # if SLAID == "" or IP == "" or ADDR == "" or QUAN == "" or PORT == "" or TIMEOUT == "":
+        #     print("error")
+        #     F1 = "error"
         while True:
-            F1 = master.execute(slave=SLAID, function_code=md.READ_HOLDING_REGISTERS, starting_address=ADDR,
-                                quantity_of_x=QUAN)
+            F1 = master.execute(slave=self.SLAID, function_code=md.READ_HOLDING_REGISTERS, starting_address=self.ADDR,
+                                quantity_of_x=self.QUAN)
             print(F1)  # 取到的所有寄存器的值
-            print(float(F1[0]/10))
-            tmp = float(F1[0]/10)
+            print(float(F1[0] / 10))
+            tmp = float(F1[0] / 10)
             tt = time.strftime("%Y%m%d %H:%M:%S", time.localtime())
             x = tt.split(" ")
             date = x[0]
             Time = x[1]
 
-            db = pymysql.connect(host='localhost', user='root', password='1qazXSW@', port=3306, db='temperature')
+            db = pymysql.connect(host='172.20.10.4', user='userj', password='1qazXSW@', port=3306, db='temperature')
 
             cursor = db.cursor()
 
             sql = 'INSERT INTO temp01(date, Time, tmp) values(%s, %s, %s)'
             try:
 
-                cursor.execute(sql,(date, Time, tmp))
+                cursor.execute(sql, (date, Time, tmp))
 
                 db.commit()
 
@@ -127,9 +132,10 @@ def Modbus():
 
             time.sleep(1)
 
-
-#Function
-
+def Modbus():
+    tmodbus = TModbus()
+    tmodbus.start()
+# Function
 
 
 frame1 = Frame(tk)
@@ -140,15 +146,10 @@ btn1 = Button(frame1, text="START", command=Modbus)
 btn1.pack(side="left", pady=40, padx=60)
 btn2 = Button(frame1, text="CONNECT", command=createConnectionWindow)
 btn2.pack(side="left", padx=40)
-btn3 = Button(frame1, text="FUNCTION", command=createFuctionWindow)
+btn3 = Button(frame1, text="FUNCTION", command=createFunctionWindow)
 btn3.pack(side="left", padx=40)
 
 tk.wm_title("MODBUS POLLING")
 tk.geometry("800x600")
 tk.mainloop()
-
-
-
-
-
 
